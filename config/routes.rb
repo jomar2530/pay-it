978e1e76b4d90fc5958512f750af97d996b56b1a
jomar2530 <jomarvalera30@gmail.com>
2024-11-02 16:44:51 +0800

    Refactor Devise routes for users to improve clarity and maintainability

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      devise_for :users, path:'', path_names: {
-        sign_in: 'login',
-        sign_out: 'logout',
-        registration: 'signup'
+      devise_for :users, path: "", path_names: {
+        sign_in: "login",
+        sign_out: "logout",
+        registration: "signup"
       },
-      controllers:{
-        sessions: 'api/v1/users/sessions',
-        registrations: 'api/v1/users/registrations'
-      }, 
+      controllers: {
+        sessions: "api/v1/users/sessions",
+        registrations: "api/v1/users/registrations"
+      },
       defaults: { format: :json }
     end
   end
